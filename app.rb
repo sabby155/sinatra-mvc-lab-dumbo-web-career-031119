b309dff15ba74d6bd8894e571aed8848cd506989
Sabrina Chowdhury <nebula155@gmail.com>
2019-04-02 17:01:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
+  
+  get '/' do 
+    erb :user_input
+  end   
+  
+  post '/' do 
+    @pig_latinizer = PigLatinizer.new(params[:user_text])
+    erb :piglatinize
+  end   
+  
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,23 @@
+class PigLatinizer 
+  attr_reader :text
+  
+  def initialize(text)
+    @text = text.downcase
+  end 
+  
+  def translate 
+  alpha = ('a'..'z').to_a
+  vowels = %w[a e i o u]
+  consonants = alpha - vowels
+
+  if vowels.include?(text[0])
+    text + 'ay'
+  elsif consonants.include?(text[0]) && consonants.include?(text[1])
+    text[2..-1] + text[0..1] + 'ay'
+  elsif consonants.include?(text[0])
+    text[1..-1] + text[0] + 'ay'
+  else
+    text # return unchanged
+  end
+  end
+end   
