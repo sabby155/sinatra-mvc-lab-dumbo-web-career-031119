2a7d1a386d0bd79507dee06e118656df9aea387b
Sabrina Chowdhury <nebula155@gmail.com>
2019-04-02 17:17:14 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,19 +5,27 @@ class PigLatinizer
    
   end 
   
-  def piglatinize(text) 
-  alpha = ('a'..'z').to_a
-  vowels = %w[a e i o u]
-  consonants = alpha - vowels
+  class PigLatinizer
 
-  if vowels.include?(text[0].downcase)
-    text + 'ay'
-  elsif consonants.include?(text[0].downcase) && consonants.include?(text[1].downcase)
-    text[2..-1] + text[0..1] + 'ay'
-  elsif consonants.include?(text[0].downcase)
-    text[1..-1] + text[0] + 'ay'
-  else
-    text # return unchanged
+  @@vowels = "aeiou"
+  @@vowels_array = @@vowels.split('')
+
+  def piglatinize(text)
+    word_arr = text.split
+    word_arr.map do |word|
+      if @@vowels.include?(word[0].downcase)
+        word + "way"
+      else
+        letters = word.split('')
+        first_vowel = letters.find {|letter|
+          @@vowels.include?(letter)}
+        fv_position = letters.find_index(first_vowel)
+        first_consonants = word[0..fv_position-1]
+        word[0..fv_position-1] = ''
+        word + first_consonants + "ay"
+      end
+    end.join(" ")
   end
-  end
+
+end
 end   
